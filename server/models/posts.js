e7458a17dc78f544eb435f2083d513cb20888a7f
jasoneffendi <jasoneffendi27@gmail.com>
2017-10-26 20:23:05 +0700

    Crud function for user

--- server/models/posts.js
+++ server/models/posts.js
@@ -46,7 +46,57 @@ function post(req,res) {
     }
 }
 
+function profile(req,res) {
+    // var opentoken = jwt.verify(req.body.token, 'fare')
+    // console.log(opentoken)
+    User.find({_id: req.query.id})
+    .then(result => {
+        res.send(result)
+    })
+}
+
+function edit(req,res) {
+    var opentoken = jwt.verify(req.body.token, 'fare')
+    console.log(opentoken)
+    User.findOne({name: req.body.name})
+    .then(user => {
+        if(req.body.user !== '' && req.body.password !== '' && req.body.email !== '' && user == null) {
+            User.findOneAndUpdate({_id: opentoken._id}, {
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email
+            })
+            .then((result,err) => {
+                if(err) return res.send(err)
+
+                res.send(result)
+            })
+        } else {
+            res.send(false)
+        }
+    })
+    .catch(err => {
+        res.send(err)
+    })
+}
+
+
+function del(req,res) {
+    var opentoken = jwt.verify(req.body.token, 'fare')
+    console.log(opentoken)
+    User.findOneAndRemove({_id: opentoken._id})
+    .then(response => {
+        res.send({
+            message: 'delete user berhasil'
+        })
+    })
+    .catch(err => {
+        res.send(err)
+    })
+}
 module.exports = {
     get,
-    post
+    post,
+    edit,
+    del
 }
